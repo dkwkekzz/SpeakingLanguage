b1d9157b46dccb3ab2801c96628fec65386e13b5
유태경 <dkwkekzz@nexon.co.kr>
2019-08-07 14:02:46 +0900

    incompany

--- Test/TestTable.cs
+++ Test/TestTable.cs
@@ -13,6 +13,9 @@ namespace Test
             Console.WriteLine($"size: {sizeof(SpeakingLanguage.Table.String16).ToString()}");
             Console.WriteLine($"size: {sizeof(SpeakingLanguage.Table.String32).ToString()}");
             Console.WriteLine($"size: {sizeof(SpeakingLanguage.Table.String64).ToString()}");
+            Console.WriteLine($"size: {sizeof(bool).ToString()}"); 
+            Console.WriteLine($"size: {sizeof(int).ToString()}");
+            Console.WriteLine($"size: {sizeof(SpeakingLanguage.Table.CommonPackage.EventPackage).ToString()}");
             Console.WriteLine($"size: {sizeof(SpeakingLanguage.Table.CommonPackage).ToString()}");
             //var sv32 = SpeakingLanguage.Table.StringHelper.Parse<SpeakingLanguage.Table.String32>("i\'m very very very power archer.");
             //var sv64 = SpeakingLanguage.Table.StringHelper.Parse<SpeakingLanguage.Table.String64>("i\'m very very very power archer. ops! good!!!");
