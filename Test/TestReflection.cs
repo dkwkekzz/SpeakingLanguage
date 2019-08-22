a56d488549abdcdc0d51612ce789ed4a61d36b3d
유태경 <dkwkekzz@nexon.co.kr>
2019-08-22 18:11:48 +0900

    리플렉션테스트 추가

---
+++ Test/TestReflection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Archer
+    {
+        public int Health { get; set; }
+        public int Power { get; }
+
+        public void Attack()
+        {
+            Console.WriteLine("Attack");
+        }
+    }
+
+    class TestReflection
+    {
+        static void Main()
+        {
+            var mths = typeof(Archer).GetMethods();
+            foreach (MethodInfo mth in mths)
+            {
+                Console.WriteLine($"mth: {mth.Name}");
+            }
+        }
+    }
+}
